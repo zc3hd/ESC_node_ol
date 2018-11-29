ddffe7ff653e6e19e69dbd070dce112a5c0d8ce1
cc <123@example.com>
2018-11-29 22:59:58 +0800

    xx

--- node_ol_007/cmd/git_to_ol/index.js
+++ node_ol_007/cmd/git_to_ol/index.js
@@ -7,7 +7,7 @@ me._cmd('git add .')
     return me._cmd('git commit -m "xx"')
   })
   .then(function() {
-    return me._cmd('git push -u name master')
+    return me._cmd('git push -u origin master')
   })
   .then(function() {
     console.log('上传git完成');
