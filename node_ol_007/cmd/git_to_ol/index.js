2ff8d97d26c6897cd83a8cd42427d97c407b9026
cc <123@example.com>
2018-11-29 23:16:06 +0800

    date:2018-11-29 23:16:05

--- node_ol_007/cmd/git_to_ol/index.js
+++ node_ol_007/cmd/git_to_ol/index.js
@@ -1,13 +1,15 @@
-var fn = require('../cmd.js');
-var me = new fn();
+var Tool = require('../tool.js');
+var tool = new Tool();
+// 获取当前时间戳
+var timestamp =Date.parse(new Date());
 
-
-me._cmd('git add ../')
+tool
+  ._cmd('git add ../')
   .then(function() {
-    return me._cmd(`git commit -m "info:${Math.random()}"`)
+    return tool._cmd(`git commit -m "date:${tool._date(timestamp)}"`)
   })
   .then(function() {
-    return me._cmd('git push -u origin master')
+    return tool._cmd('git push -u origin master')
   })
   .then(function() {
     console.log('上传git完成');
