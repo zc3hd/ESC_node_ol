66779e47b4c3fc99e4745847cd3c33d047cbe575
armincc <343371169@qq.com>
2018-11-30 09:01:40 +0800

    date:2018-11-30 09:01:40

--- demo_001/cmd/git_to_ol/index.js
+++ demo_001/cmd/git_to_ol/index.js
@@ -5,9 +5,11 @@ var tool = new Tool();
 
 // 获取当前时间戳
 var timestamp = Date.parse(new Date());
+
 // 要提交的目录
 var _url = path.join(__dirname,'../../../');
-
+// 要提交的源的名字
+var origin = 'name';
 
 
 tool
@@ -16,7 +18,7 @@ tool
     return tool._cmd(`git commit -m "date:${tool._date(timestamp)}"`)
   })
   .then(function() {
-    return tool._cmd('git push -u origin master')
+    return tool._cmd(`git push -u ${origin} master`)
   })
   .then(function() {
     console.log('上传git完成');
